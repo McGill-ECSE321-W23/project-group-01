b4fd09c7d61fff1fd97ac9634f9256fc5d4a3a10
Evan Boyd <evan.boyd@mail.mcgill.ca>
2023-03-01 12:10:15 -0800

    Fix owner test class and add extra account tests

--- PLMS/src/test/java/ca/mcgill/ecse321/PLMS/Tests/AccountRepositoryTests.java
+++ PLMS/src/test/java/ca/mcgill/ecse321/PLMS/Tests/AccountRepositoryTests.java
@@ -10,44 +10,115 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import ca.mcgill.ecse321.PLMS.model.Account;
-import ca.mcgill.ecse321.PLMS.repository.Account;
+import ca.mcgill.ecse321.PLMS.model.Employee;
+import ca.mcgill.ecse321.PLMS.model.MonthlyCustomer;
+import ca.mcgill.ecse321.PLMS.model.Owner;
+import ca.mcgill.ecse321.PLMS.repository.AccountRepository;
+import ca.mcgill.ecse321.PLMS.repository.EmployeeRepository;
+import ca.mcgill.ecse321.PLMS.repository.MonthlyCustomerRepository;
+import ca.mcgill.ecse321.PLMS.repository.OwnerRepository;
 
 @SpringBootTest
 public class AccountRepositoryTests {
     @Autowired
     private AccountRepository accountRepository;
+    @Autowired
+    private MonthlyCustomerRepository monthlyCustomerRepository;
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    @Autowired
+    private OwnerRepository ownerRepository;
 
     @BeforeEach
     @AfterEach
     public void clearDataBase(){
+        monthlyCustomerRepository.deleteAll();
+        employeeRepository.deleteAll();
+        ownerRepository.deleteAll();
         accountRepository.deleteAll();
+
     }
 
     @Test
-    public void testPersistAndLoadAccount(){
-        //=-=-=-=-=-=- Create object -=-=-=-=-=-=//
-        String email = "teta.saniya@teta.com";
-        String password = "PasswordSoSuperSecured12345";
-        String name = "Saniya";
+    public void testPersistAndLoadCustomerAccount(){
+        //=-=-=-=-=-=- Create monthly customer -=-=-=-=-=-=//
+        String mEmail = "teta.saniya@teta.com";
+        String mPassword = "PasswordSoSuperSecured12345";
+        String mName = "Saniya";
     
-        Account account = new Account();
+        MonthlyCustomer monthlyCustomer = new MonthlyCustomer();
         //Set all parameters
-        account.setEmail(email);
-        account.setPassword(password);
-        account.setName(name);
+        monthlyCustomer.setEmail(mEmail);
+        monthlyCustomer.setPassword(mPassword);
+        monthlyCustomer.setName(mName);
 
         //=-=-=-=-=-=- Save account -=-=-=-=-=-=//
-        account = ownerRepository.save(account);
-        String accountEmail = account.getEmail();
+        monthlyCustomer = accountRepository.save(monthlyCustomer);
+        monthlyCustomer = monthlyCustomerRepository.save(monthlyCustomer);
+        String accountEmail = monthlyCustomer.getEmail();
 
         //=-=-=-=-=-=- Read account -=-=-=-=-=-=//
-        account = accountRepository.findAccountByEmail(accountEmail);
+        monthlyCustomer = (MonthlyCustomer) accountRepository.findAccountByEmail(accountEmail);
 
         //=-=-=-=-=-=- Asserts -=-=-=-=-=-=//
-        assertNotNull(account);
-        assertEquals(email, account.getEmail());
-        assertEquals(password, account.getPassword());
-        assertEquals(name, account.getName());
-        
-}
+        assertNotNull(monthlyCustomer);
+        assertEquals(mEmail, monthlyCustomer.getEmail());
+        assertEquals(mPassword, monthlyCustomer.getPassword());
+        assertEquals(mName, monthlyCustomer.getName());
+    }
+    @Test
+    void testPersistAndLoadEmployeeAccount(){
+        //=-=-=-=-=-=- Create employee -=-=-=-=-=-=//
+        String eEmail = "hello123@hi.com";
+        String ePassword = "weeeee";
+        String eName = "Samer";
+
+        Employee employee = new Employee();
+        //Set all parameters
+        employee.setEmail(eEmail);
+        employee.setPassword(ePassword);
+        employee.setName(eName);
+
+        //=-=-=-=-=-=- Save account to both repositories -=-=-=-=-=-=//
+        employee = accountRepository.save(employee);
+        employee = employeeRepository.save(employee);
+        String eAccountEmail = employee.getEmail();
+
+        //=-=-=-=-=-=- Read account -=-=-=-=-=-=//
+        employee = (Employee) accountRepository.findAccountByEmail(eAccountEmail);
+
+        //=-=-=-=-=-=- Asserts -=-=-=-=-=-=//
+        assertNotNull(employee);
+        assertEquals(eEmail, employee.getEmail());
+        assertEquals(ePassword, employee.getPassword());
+        assertEquals(eName, employee.getName());
+    }
+
+    @Test
+    void testPersistAndLoadOwnerAccount(){
+        //=-=-=-=-=-=- Create owner -=-=-=-=-=-=//
+        String oEmail = "owner@dorsia.com";
+        String oPassword = "ihavetoreturnsomevideotapes";
+        String oName = "Patrick Bateman";
+    
+        Owner owner = new Owner();
+        //Set all parameters
+        owner.setEmail(oEmail);
+        owner.setPassword(oPassword);
+        owner.setName(oName);
+
+        //=-=-=-=-=-=- Save account to both repositories -=-=-=-=-=-=//
+        owner = accountRepository.save(owner);
+        owner = ownerRepository.save(owner);
+        String oAccountEmail = owner.getEmail();
+
+        //=-=-=-=-=-=- Read account -=-=-=-=-=-=//
+        owner = (Owner) accountRepository.findAccountByEmail(oAccountEmail);
+
+        //=-=-=-=-=-=- Asserts -=-=-=-=-=-=//
+        assertNotNull(owner);
+        assertEquals(oEmail, owner.getEmail());
+        assertEquals(oPassword, owner.getPassword());
+        assertEquals(oName, owner.getName());
+    }
 }
--- PLMS/src/test/java/ca/mcgill/ecse321/PLMS/Tests/OwnerRepositoryTests.java
+++ PLMS/src/test/java/ca/mcgill/ecse321/PLMS/Tests/OwnerRepositoryTests.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import ca.mcgill.ecse321.PLMS.model.Owner;
-import ca.mcgill.ecse321.PLMS.repository.Owner;
+import ca.mcgill.ecse321.PLMS.repository.OwnerRepository;
 
 @SpringBootTest
 public class OwnerRepositoryTests {
