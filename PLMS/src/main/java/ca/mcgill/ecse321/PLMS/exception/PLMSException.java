a1e4aee5a7ee1cf8a46449e580e1d86ab2399041
samerrak <samer_ak@icloud.com>
2023-03-11 10:42:57 -0500

    "Exception class"

--- PLMS/src/main/java/ca/mcgill/ecse321/PLMS/exception/PLMSException.java
+++ PLMS/src/main/java/ca/mcgill/ecse321/PLMS/exception/PLMSException.java
@@ -1,4 +1,20 @@
 package ca.mcgill.ecse321.PLMS.exception;
 
-public class PLMSException {
+import org.springframework.http.HttpStatus;
+
+public class PLMSException extends RuntimeException{
+
+    private static final long serialVersionUID = 1L;
+
+    private HttpStatus status;
+
+    public PLMSException(HttpStatus status, String message) {
+        super(message);
+
+        this.status = status;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
 }
