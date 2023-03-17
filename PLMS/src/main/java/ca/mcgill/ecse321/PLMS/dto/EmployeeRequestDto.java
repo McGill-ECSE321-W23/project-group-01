239f5fc828dafb9fc640a0b483d3ecce4a48bbec
samerrak <samer_ak@icloud.com>
2023-03-17 15:54:56 -0400

    "Service classes updated + added @Column annotation to Account + Exception class updated + DTOs"

--- PLMS/src/main/java/ca/mcgill/ecse321/PLMS/dto/EmployeeRequestDto.java
+++ PLMS/src/main/java/ca/mcgill/ecse321/PLMS/dto/EmployeeRequestDto.java
@@ -1,13 +1,32 @@
 package ca.mcgill.ecse321.PLMS.dto;
 
 import ca.mcgill.ecse321.PLMS.model.Employee;
+import jakarta.validation.constraints.*;
+import org.hibernate.validator.constraints.Range;
+import org.springframework.boot.context.properties.bind.Name;
 
 public class EmployeeRequestDto {
 
+    @NotNull
+    @NotBlank(message = "Email cannot be blank.")
+    @Email
     private String email;
+
+    @NotNull
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[!@#$%^&+=]).+$", message = "Password contains at least one uppercase, lowercase and special character [!@#$%^&+=]")
+    @Size(min = 5, max = 13, message = "Password must have 5-13 character" )
+    @NotBlank(message = "Password cannot be blank.")
     private String password;
+
+    @NotNull
+    @Pattern(regexp = "^[a-zA-Z\s]+$", message = "Name can only have letters")
+    @NotBlank(message = "Name cannot be blank.")
     private String name;
+
+    @Pattern(regexp = "^[a-zA-Z\s]+$", message = "Job title can only have letters")
     private String jobTitle;
+
+    @Range(min = 1, max = 300000)
     private double hourlyWage;
 
     public void setEmail(String aEmail)
--- PLMS/src/main/java/ca/mcgill/ecse321/PLMS/dto/OwnerRequestDto.java
+++ PLMS/src/main/java/ca/mcgill/ecse321/PLMS/dto/OwnerRequestDto.java
@@ -1,10 +1,23 @@
 package ca.mcgill.ecse321.PLMS.dto;
 import ca.mcgill.ecse321.PLMS.model.Owner;
+import jakarta.validation.constraints.*;
 
 public class OwnerRequestDto {
 
+    @NotNull
+    @NotBlank(message = "Email cannot be blank.")
+    @Email
     private String email;
+
+    @NotNull
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[!@#$%^&+=]).+$", message = "Password contains at least one uppercase, lowercase and special character [!@#$%^&+=]")
+    @Size(min = 5, max = 13, message = "Password must have 5-13 character" )
+    @NotBlank(message = "Password cannot be blank.")
     private String password;
+
+    @NotNull
+    @Pattern(regexp = "^[a-zA-Z\s]+$", message = "Name can only have letters")
+    @NotBlank(message = "Name cannot be blank.")
     private String name;
 
     public void setEmail(String aEmail)
