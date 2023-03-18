fcae1b2ad8a2797c2ee0781737d3f5c9c0b5e8af
Emilien-T <emilien.taisne@mail.mcgill.ca>
2023-03-18 12:36:36 -0400

    adding MonthlyCustomerService to this branch

--- PLMS/src/main/java/ca/mcgill/ecse321/PLMS/service/MonthlyCustomerService.java
+++ PLMS/src/main/java/ca/mcgill/ecse321/PLMS/service/MonthlyCustomerService.java
@@ -1,5 +1,46 @@
 package ca.mcgill.ecse321.PLMS.service;
 
+import ca.mcgill.ecse321.PLMS.exception.PLMSException;
+import ca.mcgill.ecse321.PLMS.model.MonthlyCustomer;
+import ca.mcgill.ecse321.PLMS.repository.MonthlyCustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
 public class MonthlyCustomerService {
-    
+
+    @Autowired
+    MonthlyCustomerRepository monthlyCustomerRepository;
+
+    @Transactional
+    public Iterable<MonthlyCustomer> getAllMonthlyCustomers() { return monthlyCustomerRepository.findAll(); }
+
+    @Transactional
+    public MonthlyCustomer getMonthlyCustomerByEmail(String email) {
+        MonthlyCustomer monthlyCustomer = monthlyCustomerRepository.findMonthlyCustomerByEmail(email);
+        if (monthlyCustomer == null) {
+            throw new PLMSException(HttpStatus.NOT_FOUND, "Owner not found.");
+        }
+        return monthlyCustomer;
+    }
+
+    @Transactional
+    public MonthlyCustomer updateMonthlyCustomer(MonthlyCustomer monthlyCustomer)
+    {
+        return monthlyCustomerRepository.save(getMonthlyCustomerByEmail(monthlyCustomer.getEmail()));
+    }
+
+    @Transactional
+    public MonthlyCustomer createMonthlyCustomerAccount(MonthlyCustomer monthlyCustomer) {
+        // Create the account
+        if (monthlyCustomerRepository.findMonthlyCustomerByEmail(monthlyCustomer.getEmail()) == null)
+            return monthlyCustomerRepository.save(monthlyCustomer);
+        else
+            throw new PLMSException(HttpStatus.CONFLICT, "Account with this email already exists");
+
+    }
+
+
 }
