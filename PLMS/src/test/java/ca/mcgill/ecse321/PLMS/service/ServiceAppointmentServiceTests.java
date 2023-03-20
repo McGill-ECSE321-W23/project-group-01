a949de4f7b169821777e9b5dcf2202977185b7c2
Evan Boyd <evan.boyd@mail.mcgill.ca>
2023-03-19 21:44:51 -0400

    Test w/o account creation

--- PLMS/src/test/java/ca/mcgill/ecse321/PLMS/service/ServiceAppointmentServiceTests.java
+++ PLMS/src/test/java/ca/mcgill/ecse321/PLMS/service/ServiceAppointmentServiceTests.java
@@ -16,10 +16,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 
 import ca.mcgill.ecse321.PLMS.exception.PLMSException;
+import ca.mcgill.ecse321.PLMS.model.Employee;
+import ca.mcgill.ecse321.PLMS.model.ParkingLot;
 import ca.mcgill.ecse321.PLMS.model.Service;
 import ca.mcgill.ecse321.PLMS.model.ServiceAppointment;
 import ca.mcgill.ecse321.PLMS.repository.EmployeeRepository;
 import ca.mcgill.ecse321.PLMS.repository.MonthlyCustomerRepository;
+import ca.mcgill.ecse321.PLMS.repository.ParkingLotRepository;
 import ca.mcgill.ecse321.PLMS.repository.ServiceAppointmentRepository;
 
 @SpringBootTest
@@ -32,6 +35,9 @@ public class ServiceAppointmentServiceTests {
 
   @Mock
   private MonthlyCustomerRepository monthlyCustomerRepository;
+
+  @Mock
+  private ParkingLotRepository parkingLotRepository;
 
   @InjectMocks
   private ServiceAppointmentService serviceAppointmentService;
@@ -81,12 +87,61 @@ public class ServiceAppointmentServiceTests {
 
   @Test
   public void testGetInvalidAppointment(){
-    // //=-=-=-=-=-=- Create object -=-=-=-=-=-=//
+    // random id for testing
     int id = 4;
     when(serviceAppointmentRepository.findServiceAppointmentById(4)).thenReturn(null);
     PLMSException e = assertThrows(PLMSException.class,
 				() -> serviceAppointmentService.findServiceAppointmentById(id));
 		assertEquals(HttpStatus.NOT_FOUND, e.getStatus());
 		assertEquals("Service appointment with ID " + id + " does not exist.", e.getMessage());
+  }
+
+  @Test
+  public void createValidAppointmentWithoutAccount(){
+    // //=-=-=-=-=-=- Create object -=-=-=-=-=-=//
+    String serviceName = "30 min Car Wash";
+    int serviceCost = 30;
+    double serviceLengthInHours = 0.5;
+    Service service = new Service(serviceName, serviceCost, serviceLengthInHours);
+    //normal parameters
+    Date date = Date.valueOf("2023-02-21");
+    Time startTime = Time.valueOf("12:00:00");
+    Time endTime = Time.valueOf("12:45:00");
+    Time endTime2 = Time.valueOf("12:30:00");
+    String email = "jeff.jeff@jeff.com";
+    String password = "PasswordSuperSecured12345";
+    String name = "Jeff";
+    String jobDescription = "Porter or something like that, im not sure how to describe that job but this is a job description";
+    int hourlyWage = 15;
+    Employee jeff = new Employee(email, password, name, jobDescription, hourlyWage);
+
+
+    Time openingTime = Time.valueOf("6:00:00");
+    Time closingTime = Time.valueOf("22:00:00");
+    double smallSpotFee = 3.5;
+    double largeSpotFee = 4.5;
+    double smallSpotMonthlyFlatFee = 150;
+    double largeSpotMonthlyFlatFee = 150;
+    int id = 10;
+    ParkingLot parkingLot = new ParkingLot(openingTime, closingTime, smallSpotFee, largeSpotFee, smallSpotMonthlyFlatFee, largeSpotMonthlyFlatFee);
+    //--------------------------------//
+
+    
+    // The parking lot repo should return a single parking lot
+    when(parkingLotRepository.findAll()).thenReturn(Collections.singletonList(parkingLot));
+    when(employeeRepository.findAll()).thenReturn(Collections.singletonList(jeff));
+    ServiceAppointment appt = new ServiceAppointment(date, startTime, endTime, service);
+    ServiceAppointment appt2 = new ServiceAppointment(date, startTime, endTime2, service);
+    appt2.setEmployee(jeff);
+    when(serviceAppointmentRepository.save(appt)).thenReturn(appt2);
+
+    ServiceAppointment output = serviceAppointmentService.createServiceAppointment(appt);
+
+    assertEquals(date, output.getDate());
+    assertEquals(startTime, output.getStartTime());
+    assertEquals(endTime2, output.getEndTime());
+    assertEquals(serviceName, output.getService().getServiceName());
+    assertEquals(name, output.getEmployee().getName());
+
   }
 }
