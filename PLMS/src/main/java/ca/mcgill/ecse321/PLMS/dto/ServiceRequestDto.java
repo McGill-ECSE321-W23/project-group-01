b7935368d84b2237e4d7fdbeaa447d0e745d7578
karimosabbagh <karim.alsabbagh@mail.mcgill.ca>
2023-03-21 19:55:31 -0400

    Added ServiceRequestDto file

---
+++ PLMS/src/main/java/ca/mcgill/ecse321/PLMS/dto/ServiceRequestDto.java
@@ -0,0 +1,32 @@
+package ca.mcgill.ecse321.PLMS.dto;
+import ca.mcgill.ecse321.PLMS.model.Service;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public class ServiceRequestDto {
+    @NotNull
+    @NotBlank(message = "Service name cannot be blank.")
+    private String serviceName;
+    @Min(value = 0, message = "Cost cannot be a negative number.")
+    private double cost;
+    @Min(value = 0, message = "Length cannot be a negative number.")
+    private double lengthInHours;
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public void setLengthInHours(double lengthInHours) {
+        this.lengthInHours = lengthInHours;
+    }
+
+    public Service toModel(){
+        return new Service(serviceName, cost, lengthInHours);
+    }
+}
