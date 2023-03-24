89b00d743d3b3a6a1019859bf0449772688b575c
Walid Aissa <walidaissa047@gmail.com>
2023-03-24 14:01:43 -0400

    added invalid monthly pass by monthly customer tests

--- PLMS/src/test/java/ca/mcgill/ecse321/PLMS/service/MonthlyPassServiceTests.java
+++ PLMS/src/test/java/ca/mcgill/ecse321/PLMS/service/MonthlyPassServiceTests.java
@@ -335,6 +335,104 @@ public class MonthlyPassServiceTests {
         assertEquals(output.getCustomer(), monthlyCustomer);
     }
 
+    @Test
+    public void testGetInvalidMonthlyPassesByMonthlyCustomer1(){
+        double fee = 50.50;
+        String spotNumber = "A24";
+        String licensePlate = "123ABC123";
+        Date startDate = Date.valueOf("2023-02-21");
+        Date endDate = Date.valueOf("2023-03-20");
+        boolean isLarge = true;
+        String confirmationCode = "NeverGonnaGiveYouUp";
+        int id = 1;
+
+        double fee2 = 50.50;
+        String spotNumber2 = "A25";
+        String licensePlate2 = "123ABC124";
+        Date startDate2 = Date.valueOf("2023-02-22");
+        Date endDate2 = Date.valueOf("2023-03-21");
+        boolean isLarge2 = true;
+        String confirmationCode2 = "NeverGonnaGiveYouUp";
+        int id2 = 2;
+    
+    
+        String invalidEmail = "rick.roll@gmail.com";
+        
+        MonthlyPass monthlyPass = new MonthlyPass();
+        monthlyPass.setFee(fee);
+        monthlyPass.setSpotNumber(spotNumber);
+        monthlyPass.setConfirmationCode(confirmationCode);
+        monthlyPass.setIsLarge(isLarge);
+        monthlyPass.setStartDate(startDate);
+        monthlyPass.setEndDate(endDate);
+        monthlyPass.setLicensePlate(licensePlate);
+
+        MonthlyPass monthlyPass2 = new MonthlyPass();
+        monthlyPass2.setFee(fee2);
+        monthlyPass2.setSpotNumber(spotNumber2);
+        monthlyPass2.setConfirmationCode(confirmationCode2);
+        monthlyPass2.setIsLarge(isLarge2);
+        monthlyPass2.setStartDate(startDate2);
+        monthlyPass2.setEndDate(endDate2);
+        monthlyPass2.setLicensePlate(licensePlate2);
+
+        ArrayList<MonthlyPass> monthlyPasses = new ArrayList<>();
+        monthlyPasses.add(monthlyPass1);
+        monthlyPasses.add(monthlyPass2);
+
+        when(MonthlyPassRepo.findAll()).thenReturn(monthlyPasses);
+        when(monthlyCustomerRepo.findMonthlyCustomerByEmail(email)).thenReturn(null);
+
+        
+        PLMSException e = assertThrows(PLMSException.class, () -> monthlyPassService.getMonthlyPassesByMonthlyCustomer(email));
+        assertEquals(e.getStatus(), HttpStatus.NOT_FOUND);
+        assertEquals(e.getMessage(), "The account with email " + email + " does not exist.");
+    }
+
+    @Test
+    public void testGetInvalidMonthlyPassesByMonthlyCustomer2(){
+        
+    
+        String email = "rick.roll@gmail.com";
+        String password = "intelliJLover123";
+        String name = "Samer Abdulkarim";
+        MonthlyCustomer monthlyCustomer = new MonthlyCustomer();
+        monthlyCustomer.setEmail(email);
+        monthlyCustomer.setPassword(password);
+        monthlyCustomer.setName(name);
+
+        String email2 = "no.pass@gmail.com";
+        String password2 = "VsCodeLover123";
+        String name2 = "Karl Bridi";
+        MonthlyCustomer monthlyCustomer2 = new MonthlyCustomer();
+        monthlyCustomer2.setEmail(email2);
+        monthlyCustomer2.setPassword(password2);
+        monthlyCustomer2.setName(name2);
+        
+        MonthlyPass monthlyPass = new MonthlyPass();
+        monthlyPass.setFee(fee);
+        monthlyPass.setSpotNumber(spotNumber);
+        monthlyPass.setConfirmationCode(confirmationCode);
+        monthlyPass.setIsLarge(isLarge);
+        monthlyPass.setStartDate(startDate);
+        monthlyPass.setEndDate(endDate);
+        monthlyPass.setLicensePlate(licensePlate);
+        monthlyPass.setCustomer(monthlyCustomer);
+
+        ArrayList<MonthlyPass> monthlyPasses = new ArrayList<>();
+        monthlyPasses.add(monthlyPass1);
+
+        when(MonthlyPassRepo.findAll()).thenReturn(monthlyPasses);
+        when(monthlyCustomerRepo.findMonthlyCustomerByEmail(email2)).thenReturn(monthlyCustomer2);
+
+        PLMSException e = assertThrows(PLMSException.class, () -> monthlyPassService.getMonthlyPassesByMonthlyCustomer(email2));
+        assertEquals(e.getStatus(), HttpStatus.NOT_FOUND);
+        assertEquals(e.getMessage(), "There are no monthly passes for " + monthlyCustomer2.getEmail());
+    }
+
+    
+
+
     
 
 
