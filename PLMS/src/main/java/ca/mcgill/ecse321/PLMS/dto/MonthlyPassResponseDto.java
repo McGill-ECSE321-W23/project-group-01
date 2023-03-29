9cc032bd7a036245875e699d56647e957aaa3370
Emilien-T <emilien.taisne@mail.mcgill.ca>
2023-03-29 04:51:36 -0400

    added documentation to make our swaggerUI look better

--- PLMS/src/main/java/ca/mcgill/ecse321/PLMS/dto/MonthlyPassResponseDto.java
+++ PLMS/src/main/java/ca/mcgill/ecse321/PLMS/dto/MonthlyPassResponseDto.java
@@ -3,20 +3,30 @@ package ca.mcgill.ecse321.PLMS.dto;
 import java.sql.Date;
 
 import ca.mcgill.ecse321.PLMS.model.MonthlyPass;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 
 public class MonthlyPassResponseDto {
 
-
+    @Schema(example = "5", description = "The id of the monthly pass")
     private Integer id;
+    @Schema(example= "1.25", description = "The fee assiated to the pass")
     private Double fee;
+    @Schema(example= "A24", description = "The spot number")
     private String spotNumber;
+    @Schema(example= "JK95HO95T3", description = "Code that confirms the payment (will be null before payment)")
     private String confirmationCode;
+    @Schema(example= "T3ST41", description = "License plate of the car linked to this pass")
     private String licensePlate;
+    @Schema(example= "false", description = "Is the spot a spot that can hold large vehicles")
     private Boolean isLarge;
+    @Schema(example= "2024-05-05", description = "Start date of the pass")
     private Date startDate;
+    @Schema(example= "2024-06-05", description = "End date of the pass")
     private Date endDate;
+    @Schema(example= "1", description = "Floor number where the spot is located at")
     private Integer floorNumber;
+    @Schema(example= "user@email.com", description = "Email linked to the account of the customer")
     private String monthlyCustomerEmail;
 
     /**
--- PLMS/src/main/java/ca/mcgill/ecse321/PLMS/dto/ServiceResponseDto.java
+++ PLMS/src/main/java/ca/mcgill/ecse321/PLMS/dto/ServiceResponseDto.java
@@ -1,10 +1,14 @@
 package ca.mcgill.ecse321.PLMS.dto;
 
 import ca.mcgill.ecse321.PLMS.model.Service;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public class ServiceResponseDto {
+    @Schema(example= "Wheel Replacement", description = "Name of the service")
     private String serviceName;
+    @Schema(example= "50.25", description = "Cost of the service")
     private double cost;
+    @Schema(example= "2.0", description = "The estimated amount of time it will take to perform the service")
     private double lengthInHours;
 
     public ServiceResponseDto(Service service) {
