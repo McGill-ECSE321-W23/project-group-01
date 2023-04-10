b6e6908398f34481b6f86c5a36584ee55bb9866a
Kalamar136 <karl.bridi@mail.mcgill.ca>
2023-04-10 14:53:46 -0400

    Fixed home page entry

--- PLMS-Frontend/src/router/index.js
+++ PLMS-Frontend/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
 import GeneralCreateGuestPass from '@/components/GeneralCreateGuestPass'
 import InternalCreateMonthlyPass from '@/components/InternalCreateMonthlyPass'
 import Home from '@/components/Home'
@@ -27,8 +26,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Home',
+      component: Home
     },
     {
       path: '/createGuestPass',
@@ -72,12 +71,6 @@ export default new Router({
       name: 'ViewGuestPasses',
       component: ViewGuestPasses
     },
-    {
-      path: '/home',
-      name: 'Home',
-      component: Home
-    },
-
     {
       path: '/login-user',
       name: 'LoginUser',
