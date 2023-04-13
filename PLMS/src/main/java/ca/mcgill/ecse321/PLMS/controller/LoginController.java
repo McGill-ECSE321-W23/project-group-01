a35de378a86d2c9a1b1a8c4859b4557956844262
Emilien-T <emilien.taisne@mail.mcgill.ca>
2023-04-13 18:32:54 -0400

    added comments to the LoginController Class

--- PLMS/src/main/java/ca/mcgill/ecse321/PLMS/controller/LoginController.java
+++ PLMS/src/main/java/ca/mcgill/ecse321/PLMS/controller/LoginController.java
@@ -7,12 +7,19 @@ import ca.mcgill.ecse321.PLMS.dto.OwnerResponseDto;
 import ca.mcgill.ecse321.PLMS.service.EmployeeService;
 import ca.mcgill.ecse321.PLMS.service.MonthlyCustomerService;
 import ca.mcgill.ecse321.PLMS.service.OwnerService;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
+/**
+ * Controller class related to endpoints for logging in (or verification of the account credentials), in the context of the PLMS system
+ */
 @CrossOrigin(origins = "*")
 @RestController
 public class LoginController {
@@ -24,15 +31,18 @@ public class LoginController {
     @Autowired
     private EmployeeService employeeService;
 
-//    @GetMapping("/login")
-//    public ResponseEntity<?> getUser(@RequestBody AccountRequestDto accountRequest){
-//        if (accountRequest.getUser().equals("Owner"))
-//            return new ResponseEntity<OwnerResponseDto>(new OwnerResponseDto(ownerService.getOwnerByEmailAndPassword(accountRequest.getEmail(), accountRequest.getPassword())), HttpStatus.OK);
-//        if (accountRequest.getUser().equals("Employee"))
-//            return new ResponseEntity<EmployeeResponseDto>(new EmployeeResponseDto(employeeService.getEmployeeByEmailAndPassword(accountRequest.getEmail(), accountRequest.getPassword())), HttpStatus.OK);
-//        else
-//            return new ResponseEntity<MonthlyCustomerResponseDto>(new MonthlyCustomerResponseDto(monthlyCustomerService.getMonthlyCustomerByEmailAndPassword(accountRequest.getEmail(), accountRequest.getPassword())), HttpStatus.OK);
-
+    /**
+     * Controller Method that allows the verfication of the email and password of an owner, customer or an employee.
+     * 
+     * @param user Either "Owner", "Employee" or "Customer" depending on what account you want to log into
+     * @param email Email of the account
+     * @param password Password of the account
+     * @return
+     */
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200"),
+        @ApiResponse(responseCode = "404", description = "Owner not found. |OR| Monthly customer not found. |OR| Employee not found. |OR| Please enter the correct password", content = {@Content(mediaType = "String")})
+    })
     @GetMapping("/login/{user}")
     public ResponseEntity<?> getUser(@PathVariable String user, @RequestParam String email, @RequestParam String password){
         if (user.equals("Owner"))
